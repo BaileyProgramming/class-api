6e60cebfdff0535d318900437c4a7bab233410b7
Unknown <beetle442002@gmail.com>
2018-01-22 13:45:53 -0600

    added get classes

--- public/index.php
+++ public/index.php
@@ -5,11 +5,71 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
 
-    return $response;
+$app->get('/classes', 'getClasses');  //Gets all names
+
+$app->get('/classes/{term}', function ($request){
+        $term = $request->getAttribute('term');
+	require '../conn.php';
+        $sql="SELECT * FROM classschedule WHERE term = '". $term . "'";
+	$rs=$conn->query($sql);
+	 $rs=$conn->query($sql);
+ while($arr = $rs->fetch_array(MYSQLI_ASSOC)){
+             if($arr == null){
+                     echo json_encode(array(
+                             "error" => 1,
+                             "message" => "Error retreaving classes!",
+                     ));
+                     return;
+             }else{
+                     $rows[] = $arr;
+             }
+     }
+
+ echo json_encode($rows);
+
 });
+
+
 $app->run();
 
+
+function getClasses(){
+	require '../conn.php';
+	$sql="SELECT * FROM classschedule";
+    $rs=$conn->query($sql);
+    while($arr = $rs->fetch_array(MYSQLI_ASSOC)){
+		if($arr == null){
+			echo json_encode(array(
+				"error" => 1,
+				"message" => "Error retreaving classes!",
+			));
+			return;
+		}else{
+			$rows[] = $arr;
+		}
+	}
+	
+    echo json_encode($rows);
+}
+
+
+function getClassesByTerm($request){
+	$term = $request->getParm('term');
+	require '../conn.php';
+	$sql="SELECT * FROM classschedule WHERE term = '". $term . "'";
+    $rs=$conn->query($sql);
+    while($arr = $rs->fetch_array(MYSQLI_ASSOC)){
+		if($arr == null){
+			echo json_encode(array(
+				"error" => 1,
+				"message" => "Error retreaving classes!",
+			));
+			return;
+		}else{
+			$rows[] = $arr;
+		}
+	}
+	
+    echo json_encode($rows);
+}
