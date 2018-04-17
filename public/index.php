c7fa2c52f7d9aafa90b0bb5faf685f2b1b139f73
Unknown <beetle442002@gmail.com>
2018-04-17 17:57:55 -0500

    locations, buildings, and room Info
    Added ability to get information for locations, buildings, and rooms. (locations in a facade currently)

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$filename = "menu.json";
-
 $app = new \Slim\App;
 
 $app->get('/classes', 'getClasses');  //Gets all names
@@ -32,13 +30,22 @@ $app->get('/classes/{term}', function ($request){
 
 });
 
-$app->post('/menu', function(){
-	$result = @file_get_contents("php://input");;
-//	fwrite(STDOUT, $result);
-//	$obj = json_decode($result, true);  
-//	echo '<p>' . $obj . '</p>';
-	save_menu_item($result);//	print_r($obj);
+$app->get('/locations[/{location}[/{building}[/{room}]]]', function ($request){
+        $location = $request->getAttribute('location');
+        $building = $request->getAttribute('building');
+        $room = $request->getAttribute('room');
+//      echo "Location - " . $location . "<br />Building - " . $building . "<br />Room - " . $room;
+        if ($building == null){
+                getbuildings();
+        }else{
+                if($room == null){
+                getrooms($building);
+                }else{ 
+                        getroom($building, $room);
+                }
+        }
 });
+
 
 
 $app->run();
@@ -84,79 +91,62 @@ function getClassesByTerm($request){
     echo json_encode($rows);
 }
 
-
-function save_menu_item($obj){
-	 // open the file if present
-        $handle = @fopen($GLOBALS['filename'], 'w+');
-        // create the file if needed
-        if ($handle == null)
-        {
-                $handle = fopen($GLOBALS['filename'], 'w+');
+function getbuildings(){
+        require '../conn.php';
+        $sql="SELECT * FROM buildings";
+        $rs=$conn->query($sql);
+         $rs=$conn->query($sql);
+         while($arr = $rs->fetch_array(MYSQLI_ASSOC)){
+             if($arr == null){
+                     echo json_encode(array(
+                             "error" => 1,
+                             "message" => "Error retreaving buildings!",
+                     ));
+                     return;
+             }else{
+                     $rows[] = $arr;
+             }
         }
-
-        if ($handle)
-        {
-                fwrite($handle, ($obj));
-                        // close the handle on the file
-                        fclose($handle);
-        }
+ echo json_encode($rows);
 
 }
 
+function getrooms($building){
+        require '../conn.php';
+        $sql="select * FROM rooms left join buildings on rooms.bldgid = buildings.bldgid WHERE buildings.bldgcode = '" . $building . "'";
+        $rs=$conn->query($sql);
+         $rs=$conn->query($sql);
+         while($arr = $rs->fetch_array(MYSQLI_ASSOC)){
+             if($arr == null){
+                     echo json_encode(array(
+                             "error" => 1,
+                             "message" => "Error retreaving buildings!",
+                     ));
+                     return;
+             }else{
+                     $rows[] = $arr;
+             }
+        }
+ echo json_encode($rows);
 
-function append_json_menu($data){
-	readmenu();
-	//print_r($GLOBALS['data']);
-	//echo("<br />");
-	//print_r($GLOBALS['json']);
-	$jdata= $GLOBALS['json'];
-	$thedate = $GLOBALS['data'][0];
-	//echo("<br />");
-	//echo ($thedate);
-	//print_r($jdata);
-//	echo("<br />");
-//	echo("<br />");
-	$object_array = array();
-	foreach($jdata as $key => $item){		
-		if ($item[0] === $thedate){
-			echo("<a href='edit.php'>That date already has an entry.</a>");
-			exit();
-		}else{
-			array_push($object_array, $item);
-		}
-		//array_push($object_array, $item[0]);
-//      if (($item[0] === $thedate[0]) || ($key == $objindex)){
-//		  //print_r($data);
-//		  $jdata[$key] = $data;
-	  }
-	array_push($object_array, $GLOBALS['data']);
-	//
-	$temp_array = array();
-	foreach($object_array as $key => $item){
-		$temp_array[$key] = $item[0][0];
-	}
-	array_multisort($temp_array, SORT_DESC, $object_array);
-	//print_r(json_encode($GLOBALS['json']));      
-   
-	//print_r($object_array);
-//	
-//	
-//	
-	// open the file if present
-	$handle = @fopen($GLOBALS['filename'], 'w+');
-	// create the file if needed
-	if ($handle == null)
-	{
-		$handle = fopen($GLOBALS['filename'], 'w+');
-	}
+}
 
-	if ($handle)
-	{
-		fwrite($handle, json_encode($object_array));
-			// close the handle on the file
-			fclose($handle);
-	}
-	
-}//---------End of append_json_menu()
+function getroom($building, $room){
+        require '../conn.php';
+        $sql="select * FROM rooms left join buildings on rooms.bldgid = buildings.bldgid WHERE (buildings.bldgcode = '" . $building . "') AND (rooms.roomnumber = '" . $room . "')";
+        $rs=$conn->query($sql);
+         $rs=$conn->query($sql);
+         while($arr = $rs->fetch_array(MYSQLI_ASSOC)){
+             if($arr == null){
+                     echo json_encode(array(
+                             "error" => 1,
+                             "message" => "Error retreaving buildings!",
+                     ));
+                     return;
+             }else{
+                     $rows[] = $arr;
+             }
+        }
+ echo json_encode($rows);
 
-
+}
